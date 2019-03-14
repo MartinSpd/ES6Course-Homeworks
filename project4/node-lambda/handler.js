dcf4301f6414efb6d2940f7dd5e21de721df9379
martin.spodniak <martin.spodniak@accenture.com>
2019-03-14 15:51:41 +0100

    WIP: callback errored

--- project4/node-lambda/handler.js
+++ project4/node-lambda/handler.js
@@ -8,20 +8,22 @@
 module.exports.calculate = async (event, context, callback) => {
 
   const number = event.pathParameters.number;
+  const message = `Second square of number ${
+    number} is ${(!isNaN(number) ? Math.pow(
+    number, 2) : 0)}.`;
   
   const successMessage = {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Second square of number ${number} is ${(
-        !isNaN(number) ? Math.pow(number, 2) : 0)}.`
+      message: message
     })
   };
-  const errorMessage = {
-    statusCode: 400,
-    body: JSON.stringify({
-      message: err.message
-    })
-  };
+  // const errorMessage = {
+  //   statusCode: 400,
+  //   body: JSON.stringify({
+  //     message: err.message
+  //   })
+  // };
   return successMessage;
   // callback(errorMessage, successMessage);
 };
