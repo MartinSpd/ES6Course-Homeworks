0ce0683527593158d01bfb638211817b3c07d50f
martin.spodniak <martin.spodniak@accenture.com>
2019-03-14 14:35:46 +0100

    edited lambda

--- project4/node-lambda/handler.js
+++ project4/node-lambda/handler.js
@@ -3,14 +3,13 @@
 module.exports.calculate = async (event, context) => {
 
   const number = 2;
-  // // const square = ;
-  // const message = `Second square of number ${number} is 
-  //   ${((!number.isNaN()) ? Math.pow(number, 2) : 0)}`;
+  const message = `Second square of number ${number} is 
+    ${(!isNaN(number) ? Math.pow(number, 2) : 0)}`;
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Second square of number ${number} is ${Math.pow(number, 2)}`,
+      message: message,
       input: event,
     }),
   };
