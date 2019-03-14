b1a12c1cdcdf04f935d44af016070098c5835113
martin.spodniak <martin.spodniak@accenture.com>
2019-03-14 12:44:56 +0100

    WIP: extending lambda function with text-only message

--- project4/node-lambda/handler.js
+++ project4/node-lambda/handler.js
@@ -2,15 +2,15 @@
 
 module.exports.calculate = async (event, context) => {
 
-  const number = 2;
-  // const square = ;
-  const message = `Second square of number ${number} is 
-    ${((!number.isNaN()) ? Math.pow(number, 2) : 0)}`;
+  // const number = 2;
+  // // const square = ;
+  // const message = `Second square of number ${number} is 
+  //   ${((!number.isNaN()) ? Math.pow(number, 2) : 0)}`;
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: message,
+      message: 'message',
       input: event,
     }),
   };
