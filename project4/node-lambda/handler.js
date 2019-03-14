0828dc1789231895a95747c623a1124e8975392d
martin.spodniak <martin.spodniak@accenture.com>
2019-03-14 16:37:05 +0100

    Project 4 done - removin g comments

--- project4/node-lambda/handler.js
+++ project4/node-lambda/handler.js
@@ -8,35 +8,14 @@
 module.exports.calculate = async (event, context, callback) => {
 
   const number = event.pathParameters.number;
-  // const message = `Second square of number ${
-  //   number} is ${(!isNaN(number) ? Math.pow(
-  //   number, 2) : 0)}.`;
-  
-  // return {
-  //   statusCode: 200,
-  //   body: JSON.stringify({
-      // message: `Second square of number ${
-      //   number} is ${(!isNaN(number) ? Math.pow(
-      //   number, 2) : 0)}.`
-  //   })
-  // };
-  // const errorMessage = {
-  //   statusCode: 400,
-  //   body: JSON.stringify({
-  //     message: err.message
-  //   })
-  // };
-
-  // try { return successMessage; } 
-  //   catch (err) { return errorMessage; }
-  
-  // return successMessage;
+  const square = (!isNaN(number) ? Math.pow(
+    number, 2) : 0);
+    
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({
       number: number,
-      square: (!isNaN(number) ? Math.pow(
-        number, 2) : 0),
+      square: square,
       message: `Second square of number ${
         number} is ${square}.`
     })
