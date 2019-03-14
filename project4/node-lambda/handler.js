de67d0886425967e2b0fec3b117033a59db80e44
martin.spodniak <martin.spodniak@accenture.com>
2019-03-14 12:34:43 +0100

    WIP: reverting lambda function

--- project4/node-lambda/handler.js
+++ project4/node-lambda/handler.js
@@ -2,13 +2,14 @@
 
 module.exports.calculate = async (event, context) => {
 
-  const number = 2;
-  const square = (!number.isNaN() ? Math.pow(number, 2) : 0);
+  // const number = 2;
+  // const square = ((!number.isNaN()) ? Math.pow(number, 2) : 0);
+  // message: `Second square of number ${number} is ${square}`,
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Second square of number ${number} is ${square}`,
+      message: 'Return text',
       input: event,
     }),
   };
