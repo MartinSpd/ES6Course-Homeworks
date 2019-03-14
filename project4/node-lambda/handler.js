0125ce914b99eefdf1c317e3478cbdb6f485e9e3
martin.spodniak <martin.spodniak@accenture.com>
2019-03-14 12:38:33 +0100

    WIP: extending lambda function with new message

--- project4/node-lambda/handler.js
+++ project4/node-lambda/handler.js
@@ -2,14 +2,15 @@
 
 module.exports.calculate = async (event, context) => {
 
-  // const number = 2;
-  // const square = ((!number.isNaN()) ? Math.pow(number, 2) : 0);
-  // message: `Second square of number ${number} is ${square}`,
+  const number = 2;
+  // const square = ;
+  const message = `Second square of number ${number} is 
+    ${((!number.isNaN()) ? Math.pow(number, 2) : 0)}`,
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Return text',
+      message: message,
       input: event,
     }),
   };
